7edddef0a8b95d2b65b07506a9dce4c94d032226
J. Langley <jameselangley@gmail.com>
2023-02-21 18:29:42 -0600

    Updated to generate mixture files.

--- tools/mixaudio.py
+++ tools/mixaudio.py
@@ -1,11 +1,18 @@
 from pydub import AudioSegment
+import os
 
-base = '/workspaces/demucs/data/moisesdb23_labelnoise_v1.0/ff486935-7ce2-4e23-8908-0ff5fcc50856'
-bass = AudioSegment.from_file(f'{base}/bass.wav')
-drums = AudioSegment.from_file(f'{base}/drums.wav')
-other = AudioSegment.from_file(f'{base}/other.wav')
-vocals = AudioSegment.from_file(f'{base}/vocals.wav')
+# base = '/home/jlangley/git/demucs/data/moisesdb23_labelnoise_v1.0/ff486935-7ce2-4e23-8908-0ff5fcc50856'
+base = '/home/jlangley/git/demucs/data/moisesdb23_labelnoise_v1.0'
 
-combined = bass.overlay(drums)
+with os.scandir(base) as entries:
+    for entry in entries:
+        print(entry.path)
+        bass = AudioSegment.from_file(f'{entry.path}/bass.wav')
+        drums = AudioSegment.from_file(f'{entry.path}/drums.wav')
+        other = AudioSegment.from_file(f'{entry.path}/other.wav')
+        vocals = AudioSegment.from_file(f'{entry.path}/vocals.wav')
 
-combined.export(f'{base}/mix.wav', format='wav')
+        combined = bass.overlay(drums)
+        combined = combined.overlay(vocals)
+        combined = combined.overlay(other)
+        combined.export(f'{entry.path}/mixture.wav', format='wav')
